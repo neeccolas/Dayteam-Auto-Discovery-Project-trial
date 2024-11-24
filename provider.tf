8a26ef025be5ebf9c81a0c9e0b4a225d739df3ca
nicmboso <devops.wizzy@gmail.com>
2024-11-24 07:40:26 +0000

    enabling token variable to auto pick token from vaultserver in provider.tf

--- provider.tf
+++ provider.tf
@@ -17,9 +17,14 @@ terraform {
 provider "vault" {
   address = "https://vault.dobetabeta.shop"
   #login to vault server and pick the token
-  token = "s.2r8yIx8G5cReQ1OvuFNrWLar"
+  token   = file("./token.txt")
+  # token = "s.q99pWTj2kQhHFCxSZ5epRXrR"
 }
 
 data "vault_generic_secret" "vault-secret" {
   path = "secret/database"
+}
+
+data "vault_generic_secret" "vault-secret-nr" {
+  path = "secret/newrelic"
 }
